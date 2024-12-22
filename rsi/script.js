abe9d363740510472b8b5f760131e75ceca80d4b
Erpan1945 <152272315+Erpan1945@users.noreply.github.com>
2024-12-22 15:34:27 +0700

    Delete rsi/script.js

--- rsi/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.btn').addEventListener('click', function() {
-    const checkIn = document.getElementById('check-in').value;
-    const checkOut = document.getElementById('check-out').value;
-    const destination = document.getElementById('destination').value;
-
-    alert(`Searching for ${destination} from ${checkIn} to ${checkOut}`);
-});
